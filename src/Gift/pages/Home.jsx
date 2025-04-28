6f8ce66032a909782f67f01cf9549bb602a7b562
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-04-28 17:10:58 +0300

    Auto-update: 2025-04-28 17:10:58

--- src/Gift/pages/Home.jsx
+++ src/Gift/pages/Home.jsx
@@ -1,5 +1,5 @@
 import homeStyles from './Home.module.css'
-import FirstMeeting from '../components/FirstMeeting'
+import NavButtons from '../components/LinkButtons'
 
 const Home = () => {
   return (
@@ -7,7 +7,10 @@ const Home = () => {
       <div className={homeStyles.background}></div>
       <div className={homeStyles.content}>
         <h1>Happy 9-Months Together! 🎉</h1>
-        {/* <FirstMeeting homeStyles={homeStyles} /> */}
+        <p>Every day with you is my favourite part ❤️</p>
+        <div className={homeStyles.buttons}>
+          <NavButtons />
+        </div>
       </div>
     </div>
   )
