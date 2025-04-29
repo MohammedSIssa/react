b7cc9fc8776ecaf4a6625974e6c820f110320cbe
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-04-29 15:59:49 +0300

    Auto-update: 2025-04-29 15:59:49

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/Gift/',
+  base: '/landing-page-1/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
