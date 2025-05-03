332570d15d9ede023e67b67dcbea080fb9804244
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-05-03 16:54:43 +0300

    Auto-update: 2025-05-03 16:54:43

---
+++ src/25-landing-pages/6-online-presense/LandingPage6.jsx
@@ -0,0 +1,10 @@
+import "./style.css";
+import Hero from "./components/Hero";
+
+export default function LandingPage6() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import { createRoot } from "react-dom/client";
 // import LandingPage1 from "./25-landing-pages/1-react-landing-page/LandingPage1";
 // import LandingPage3 from "./25-landing-pages/3-shipping-site/LandingPage-3";
 // import LandingPage4 from "./25-landing-pages/4-my-custom-design/LandingPage-4";
-import LandingPage5 from "./25-landing-pages/5-responsive-lp/LandingPage-5";
+// import LandingPage5 from "./25-landing-pages/5-responsive-lp/LandingPage-5";
+import LandingPage6 from "./25-landing-pages/6-online-presense/LandingPage6";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <Gift /> */}
     {/* <LandingPage1 /> */}
-    <LandingPage5 />
+    {/* <LandingPage5 /> */}
+    <LandingPage6 />
   </StrictMode>
 );
