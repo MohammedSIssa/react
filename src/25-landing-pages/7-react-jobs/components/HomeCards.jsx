45c625392f9c481b21a1ad2fac4ff91997aa9578
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-05-04 16:53:00 +0300

    Auto-update: 2025-05-04 16:53:00

--- src/25-landing-pages/7-react-jobs/components/HomeCards.jsx
+++ src/25-landing-pages/7-react-jobs/components/HomeCards.jsx
@@ -4,10 +4,10 @@ export default function HomeCards() {
   return (
     <div className="flex flex-col gap-5 w-full px-5 py-8 lg:px-20 bg-indigo-50 items-center justify-center lg:flex-row">
       <div className="flex flex-col gap-5 w-full h-fit lg:w-1/2 bg-slate-300 rounded-xl text-slate-900 p-5">
-        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold">
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-extrabold">
           For Developers
         </h1>
-        <p className="text-xs sm:text-lg md:text-xl">
+        <p className="text-xs sm:text-lg md:text-xl font-light">
           Browse our React jobs and start your career today
         </p>
         <NavLink
@@ -19,15 +19,15 @@ export default function HomeCards() {
         </NavLink>
       </div>
       <div className="flex flex-col gap-5 w-full h-fit lg:w-1/2 p-5 rounded-xl bg-indigo-200">
-        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold">
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-extrabold">
           For Employers
         </h1>
-        <p className="text-xs sm:text-lg md:text-xl">
+        <p className="text-xs sm:text-lg md:text-xl font-light">
           List your job to find the perfect developer for the role
         </p>
         <NavLink
           to="/add-job"
-          className="text-sm sm:text-md hover:bg-indigo-700 hover:text-indigo-50 md:text-lg lg:text-xl bg-indigo-600 text-indigo-100 w-fit px-5 py-2 font-bold rounded-lg"
+          className="text-sm sm:text-md hover:bg-indigo-700 hover:text-indigo-50 md:text-lg lg:text-xl bg-indigo-600 text-indigo-50 w-fit px-5 py-2 font-bold rounded-lg"
         >
           {" "}
           Add Job{" "}
