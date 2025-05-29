e6b956b49ac7f238b94f6bd38dbd2bf9125a1efa
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-05-29 22:19:40 +0300

    Auto-update: 2025-05-29 22:19:40

--- src/mohsaid99/layouts/homepage.jsx
+++ src/mohsaid99/layouts/homepage.jsx
@@ -1,19 +1,22 @@
-import Navbar from '../components/Navbar'
-import MainContent from '../components/MainContent'
-import ErrorPage from '../components/ErrorPage'
-import ScrollToTopButton from '../components/ScrollToTopButton'
+import Navbar from "../components/Navbar";
+import MainContent from "../components/MainContent";
+import ErrorPage from "../components/ErrorPage";
+import ScrollToTopButton from "../components/ScrollToTopButton";
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col min-h-dvh" style={{ backgroundColor: 'var(--bg-color)', color: 'var(--font-color)' }}>
+    <div
+      className="flex flex-col min-h-dvh"
+      style={{ backgroundColor: "var(--bg-color)", color: "var(--font-color)" }}
+    >
       <Navbar />
       <Routes>
-        <Route path='/:type/:id' element={<MainContent />}></Route>
-        <Route path='*' element={<ErrorPage />}></Route>
+        <Route path="/:type/:id" element={<MainContent />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
       <ScrollToTopButton />
     </div>
-  )
+  );
 }
